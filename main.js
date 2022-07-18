65fbbd1b5ced5035b74ee4a6f5feb45da6388133
LucasMRomao <lmartins.romao@gmail.com>
2022-07-18 20:16:26 -0300

    Testes iniciais botões e modal

---
+++ main.js
@@ -0,0 +1,17 @@
+$(function(){
+    $("#bPadrinho").click(function(){
+        alert("Entrar como padrinho!");
+    });
+
+    $("#bConvidado").click(function(){
+        alert("Entrar como convidado!");
+    });
+
+    $("#bCancelar").click(function(){
+        $("#iSenha").val('');
+    });
+
+    $("#bEntrar").click(function(){
+        
+    });
+})
