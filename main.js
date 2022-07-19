51d8bd756ddf9afb4cf36b5433ef2cf7a5ff890c
LucasMRomao <lmartins.romao@gmail.com>
2022-07-18 21:13:25 -0300

    Teste listar presentes noivos

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 $(function(){
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyCfEVMcjvEc5aP48EGZKNGvApYBmpPxluw",
+        authDomain: "casamento-mateus.firebaseapp.com",
+        projectId: "casamento-mateus",
+        storageBucket: "casamento-mateus.appspot.com",
+        messagingSenderId: "514547738129",
+        appId: "1:514547738129:web:fc9ac8cfe7a68ad21433b0",
+        measurementId: "G-35M59W0GLJ"
+    };
+
+    if(!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
+
+    const auth = firebase.auth();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const db = firebase.firestore();
+
     $("#bPadrinho").click(function(){
         $("#dPrincipal").load("padrinho.html");
     });
@@ -12,6 +31,18 @@ $(function(){
     });
 
     $("#bEntrar").click(function(){
+        let senha = $("#iSenha").val();
 
+        db.collection('credenciais_noivos').onSnapshot((data) =>{
+            data.docs.map((val) => {
+                let dbPassword = val.data().senha;
+
+                if(senha == dbPassword){
+                    $("#dPrincipal").load("noivos.html");
+                }else{
+                    alert("Senha inválida!");
+                }
+            });
+        });
     });
 })
